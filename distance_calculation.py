e29de97fc9d7a1839ee56e6c8cf425cc40c6ca07
ants <ants@antss-MacBook-Air.local>
2026-03-12 10:24:07 -0400

    added safebox exception & changed logic of size_multiplier

--- distance_calculation.py
+++ distance_calculation.py
@@ -27,7 +27,7 @@ def calculate_zipcode_distance(sender_lat, sender_lon, receiver_lat, receiver_lo
 # takes in a zipcode and dataframe of all zip codes and returns a dataframe of the lat & longitude numbers
 def zip_to_lat_lon(zip, allzips_df):
   zip_lat_lon = allzips_df.loc[allzips_df["zip"] == zip, ["lat","lng"]]
-  print(zip_lat_lon)
+  # print(zip_lat_lon)
   return zip_lat_lon
 
 # given a row of the package dataframe, compute the distance between the sender and reciever
--- rate_calculation.py
+++ rate_calculation.py
@@ -2,24 +2,44 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-# def calculate_size_multiplier(dimensions):
+# takes in a dataframe's row and calculates the size multiplier 
+def calculate_size_multiplier(row):
+  # given a format of num*num*num, split the string 
+  dimensions = [int(x) for x in row["Size (Inch)"].split("*")]
+  # calculate the size multiplier, never less than 1: calculation is based off of just length x width for some reason, no height?
+
+  # minimum len/width is 48*40
+  if dimensions[0] > 48:
+    len = dimensions[0]
+  else:
+    len = 48 
+  if dimensions[1] > 40:
+    wid = dimensions[1]
+  else:
+    wid = 40
+  
+  return (len/48)*(wid/40)
 
 # given a row of a data frame that has the distance in miles, calculate the payable weight of the package. 
 def calculate_payable_rate(row):
   # throw an error if it doesn't have distance in miles yet 
   # had to change the csv from unit /newline count to unit space count. have to do this to the original csv now, or change it to unit_count 
 
-  # given a format of num*num*num, split the string 
-  dimensions = [int(x) for x in row["Size (Inch)"].split("*")]
-  # calculate the size multiplier, never less than 1: calculation is based off of just length x width for some reason 
-  size_multiplier = (dimensions[0]*dimensions[1]) / (48 * 40)
-  if (size_multiplier < 1):
-    size_multiplier = 1 
+  size_multiplier = calculate_size_multiplier(row)
+  if size_multiplier > 1: 
+    size_multiplier = 1.5
 
   destination_city = row["To City"]
   distance_miles = row["distance_miles"]
   pallet_count = row["Unit Count"]
   payable_rate = 0
+
+  print ("payable rate", row.name)
+  print ("size multiplier", size_multiplier)
+
+  if "SafeBox" in row["Client Reference #"] or "SAFE BOX" in row["Client Reference #"] or "SAFEBOX" in row["Client Reference #"]:
+    return 120
+    
 
   # try to simplify this one day 
   if(pallet_count > 8 ):
@@ -73,6 +93,7 @@ def calculate_payable_rate(row):
     elif(distance_miles < 51):
       payable_rate = 70 * pallet_count * size_multiplier
     elif(distance_miles < 61):
+      print (75, pallet_count, size_multiplier)
       payable_rate = 75 * pallet_count * size_multiplier
     elif(distance_miles < 71):
       payable_rate = 80 * pallet_count * size_multiplier
@@ -89,11 +110,11 @@ def calculate_payable_rate(row):
 def load_zone_zips():
   return pd.read_csv("data/Fimile-zone-zips.csv", dtype={"Postal Code": str})
 
-# takes in a row of a dataframe and returns the zone code 
+# takes in a row of a dataframe and returns the zone code based off of the reciever's zipcode. 
 def find_zone_code(row):
   zone_zip_df = load_zone_zips()
   zone_df = zone_zip_df.loc[zone_zip_df["Postal Code"] == row["To Zipcode"], ["Zone"]]
-  print(zone_df)
+  # print(zone_df)
   if zone_df.empty:
     return 0
   else: 
@@ -101,16 +122,13 @@ def find_zone_code(row):
   
 # takes in a row of a dataframe that has a zone code
 def calculate_base_rate(row):
-  # taken from payable rate calculation -- need to abstract 
-  # given a format of num*num*num, split the string 
-  dimensions = [int(x) for x in row["Size (Inch)"].split("*")]
-  # calculate the size multiplier, never less than 1: calculation is based off of just length x width for some reason 
-  size_multiplier = (dimensions[0]*dimensions[1]) / (48 * 40)
-  if (size_multiplier < 1):
-    size_multiplier = 1 
 
+  size_multiplier = calculate_size_multiplier(row)
   pallet_count = row["Unit Count"]
   base_rate = 0
+
+  print ("base rate", row.name)
+  print ("size multiplier", size_multiplier)
 
   if row["zone"] == "NJ-EDS-A":
     base_rate = 70 * pallet_count * size_multiplier
